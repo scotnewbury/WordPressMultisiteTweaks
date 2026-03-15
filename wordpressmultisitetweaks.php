0c48c3fcca6cb53158264b8f83bee74630b1b691
Scot Newbury <scot@scotnewbury.com>
2026-03-14 23:40:00 -0400

    Setting up the plugin structure

---
+++ wordpressmultisitetweaks.php
@@ -0,0 +1,15 @@
+/**
+ * Plugin Name: WordPress Multisite Tweaks
+ * Description: A group of tweaks for the WordPress admin section in a multisite environment
+ * Author:      Scot Newbury
+ * Requires     PHP: 7.4
+ * Version:     1.0.0
+ */
+
+// Set the namespace
+namespace ScotNewbury\WordPressMultisiteTweaks;
+
+// Stop execution of the plugin if it's call directly.
+if (!defined('ABSPATH')) {
+  exit;
+}
